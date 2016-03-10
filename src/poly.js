53a5b6bc302880301b07ff6da3278e02cf42059c
Tom Gasson <tom.gasson@arup.com>
2016-03-10 12:03:06 +1100

    implement multi-polygon writing

--- src/poly.js
+++ src/poly.js
@@ -9,147 +9,88 @@ module.exports.write = function writePoints(geometries, extent, shpView, shxView
     geometries.forEach(writePolyLine);
 
     function writePolyLine(coordinates, i) {
-        if (coordinates[0] !== undefined &&
-            coordinates[0][0] !== undefined &&
-            coordinates[0][0][0] !== undefined) {
-            var parts = coordinates.length
-            var memoryInc = shpI
-            var flattened = []
-            coordinates.forEach(function(polygon, i) {
-                Array.prototype.push.apply(flattened, justCoords(polygon))
+
+        var isMulti = multi(coordinates)
+        var length = isMulti ? coordinates.length : 1
+        var partIdx_acc = 0
+        var partIdx = []
+        if (isMulti){
+            coordinates.forEach(function(d){
+                partIdx.push(partIdx_acc)
+                partIdx_acc += d.length
             })
-            var contentLength = 52 + (parts * 4) + (flattened.length * 16);
-
-            console.log("flattened flattened.length coordinates", flattened, flattened.length, coordinates)
-            var featureExtent = flattened.reduce(function(extent, c) {
-                return ext.enlarge(extent, c);
-            }, ext.blank());
-
-
-            var xmin = Infinity
-            var ymin = Infinity
-            var xmax = -Infinity
-            var ymax = -Infinity
-            flattened.forEach(function(d){
-                var x = d[0]
-                var y = d[1];
-                if (y > ymax){ ymax = y }
-                if (y < ymin){ ymin = y }
-                if (x > xmax){ xmax = x }
-                if (x < xmin){ xmin = x }
-            })
-
-            // INDEX
-            shxView.setInt32(shxI, shxOffset / 2); // offset
-            shxView.setInt32(shxI + 4, contentLength / 2); // offset length
-
-            shxI += 8;
-            shxOffset += contentLength + 8;
-
-            shpView.setInt32(memoryInc, i + 1); // record number
-            memoryInc += 4
-
-            shpView.setInt32(memoryInc, contentLength / 2); // length // XXXX
-            memoryInc += 4
-
-            shpView.setInt32(memoryInc, TYPE, true); // POLYLINE=3
-            memoryInc += 4
-
-            shpView.setFloat64(memoryInc, xmin, true); // EXTENT
-            memoryInc += 8
-
-            shpView.setFloat64(memoryInc, ymin, true);
-            memoryInc += 8
-
-            shpView.setFloat64(memoryInc, xmax, true);
-            memoryInc += 8
-
-            shpView.setFloat64(memoryInc, ymax, true);
-            memoryInc += 8
-
-
-            shpView.setInt32(memoryInc, parts, true); // PARTS=t.geometry.coordinates.length
-            memoryInc += 4
-
-            shpView.setInt32(memoryInc, flattened.length, true); // POINTS
-            memoryInc += 4
-
-            for (var j = 0; j < parts; j++) {
-                shpView.setInt32(memoryInc, j, true);
-                memoryInc += 4
-            }
-
-            console.log("coordinates", coordinates)
-
-            for (var j = 0; j < parts; j++) {
-                coordinates[j].forEach(function(polygon, k) {
-                    polygon.forEach(function(coords, l) {
-                        shpView.setFloat64(memoryInc, coords[0], true); // X
-                        memoryInc += 8
-
-                        shpView.setFloat64(memoryInc, coords[1], true); // Y
-                        memoryInc += 8
-                    })
-                });
-            }
-
-            shpI = memoryInc;
         } else {
-            var flattened = justCoords(coordinates),
-                contentLength = (flattened.length * 16) + 48;
-
-            var featureExtent = flattened.reduce(function(extent, c) {
-                return ext.enlarge(extent, c);
-            }, ext.blank());
-
-            // INDEX
-            shxView.setInt32(shxI, shxOffset / 2); // offset
-            shxView.setInt32(shxI + 4, contentLength / 2); // offset length
-
-            shxI += 8;
-            shxOffset += contentLength + 8;
-
-            shpView.setInt32(shpI, i + 1); // record number
-            shpView.setInt32(shpI + 4, contentLength / 2); // length
-            shpView.setInt32(shpI + 8, TYPE, true); // POLYLINE=3
-            shpView.setFloat64(shpI + 12, featureExtent.xmin, true); // EXTENT
-            shpView.setFloat64(shpI + 20, featureExtent.ymin, true);
-            shpView.setFloat64(shpI + 28, featureExtent.xmax, true);
-            shpView.setFloat64(shpI + 36, featureExtent.ymax, true);
-            shpView.setInt32(shpI + 44, 1, true); // PARTS=1
-            shpView.setInt32(shpI + 48, flattened.length, true); // POINTS
-            shpView.setInt32(shpI + 52, 0, true); // The only part - index zero
-
-            flattened.forEach(function writeLine(coords, i) {
-                shpView.setFloat64(shpI + 60 + (i * 16), coords[0], true); // X
-                shpView.setFloat64(shpI + 60 + (i * 16) + 8, coords[1], true); // Y
-            });
-
-            shpI += contentLength + 8;
+            partIdx.push(partIdx_acc)
         }
+        var flattened = []
+
+        if (isMulti){
+            coordinates.forEach(function(part){
+                part.forEach(function(coord){
+                    flattened.push(coord)
+                })
+            })
+        } else {
+            coordinates.forEach(function(coord){
+                flattened.push(coord)
+            })
+        }
+
+        var contentLength = (flattened.length * 16) + 44 + (length * 4);
+
+        var featureExtent = flattened.reduce(function(extent, c) {
+            return ext.enlarge(extent, c);
+        }, ext.blank());
+
+        // INDEX
+        shxView.setInt32(shxI, shxOffset / 2); // offset
+        shxView.setInt32(shxI + 4, contentLength / 2); // offset length
+
+        shxI += 8;
+        shxOffset += contentLength + 8;
+
+        shpView.setInt32(shpI, i + 1); // record number
+        shpView.setInt32(shpI + 4, contentLength / 2); // length
+        shpView.setInt32(shpI + 8, TYPE, true); // POLYLINE=3
+        shpView.setFloat64(shpI + 12, featureExtent.xmin, true); // EXTENT
+        shpView.setFloat64(shpI + 20, featureExtent.ymin, true);
+        shpView.setFloat64(shpI + 28, featureExtent.xmax, true);
+        shpView.setFloat64(shpI + 36, featureExtent.ymax, true);
+        shpView.setInt32(shpI + 44, length, true); // PARTS=1
+        shpView.setInt32(shpI + 48, flattened.length, true); // POINTS
+
+        var idx = shpI + 52
+        for (var i = 0; i < partIdx.length; i++){
+            shpView.setInt32(idx, partIdx[i], true); // The only part - index zero
+            idx += 4
+        }
+
+        flattened.forEach(function writeLine(coords, i) {
+            shpView.setFloat64(idx, coords[0], true); // X
+            idx += 8
+            shpView.setFloat64(idx, coords[1], true); // Y
+            idx += 8
+        });
+
+        shpI += contentLength + 8;
     }
 };
 
 module.exports.shpLength = function(geometries) {
-    var size = 0;
-    geometries.forEach(function(coordinates){
-        var parts = 1
-        var polySize = 0;
-        if (coordinates[0] !== undefined &&
-            coordinates[0][0] !== undefined &&
-            coordinates[0][0][0] !== undefined) {
-            parts = coordinates.length
-        }
-        var flattened = []
-        coordinates.forEach(function(polygon, i) {
-            Array.prototype.push.apply(flattened, justCoords(polygon))
-        })
-        polySize = 52 + (parts * 4) + (flattened.length * 16);
-        size += polySize
+    var size = 0
+    geometries.forEach(function(d){
+        size += getSize(d)
     })
-    console.log('calculated size', size)
     return size
 };
+
+
+function getSize(coordinates){
+    var isMulti = multi(coordinates)
+    var length = isMulti ? coordinates.length : 1
+    var flattened = justCoords(coordinates);
+    return 8 + 44 + (length * 4) + (flattened.length * 16)
+}
 
 module.exports.shxLength = function(geometries) {
     return geometries.length * 8;
@@ -160,6 +101,13 @@ module.exports.extent = function(coordinates) {
         return ext.enlarge(extent, c);
     }, ext.blank());
 };
+
+function multi(coordinates){
+    if (coordinates[0]&&coordinates[0][0]&&coordinates[0][0][0]){
+        return true
+    }
+    return false
+}
 
 function totalPoints(geometries) {
     var sum = 0;
--- test/extent.test.js
+++ test/extent.test.js
@@ -1,6 +1,8 @@
 var expect = require('expect.js'),
     extent = require('../src/extent');
-var write = require('../src/write')
+var write = require('../src/write');
+var types = require('../src/types');
+var fs = require('fs')
 
 describe('extent', function() {
 
@@ -39,57 +41,279 @@ describe('extent', function() {
         });
     });
 
-    describe('multipolygon', function() {
-        it('writes a multipolygon', function() {
-            
-            // 3 level coords
-            var points =  [
+    describe('polygon', function(){
+        var points =  [
+            [
+                149.2822265625,
+                -31.222197032103185
+            ],
+            [
+                150.00732421875,
+                -32.30570601389429
+            ],
+            [
+                147.89794921875,
+                -32.34284135639301
+            ],
+            [
+                149.2822265625,
+                -31.222197032103185
+            ]
+        ];
+
+        var xmin = Infinity
+        points.forEach(function(d){ xmin = d[0] < xmin ? d[0] : xmin})
+        var ymin = Infinity
+        points.forEach(function(d){ ymin = d[1] < ymin ? d[1] : ymin})
+        var xmax = -Infinity
+        points.forEach(function(d){ xmax = d[0] > xmax ? d[0] : xmax})
+        var ymax = -Infinity
+        points.forEach(function(d){ ymax = d[1] > ymax ? d[1] : ymax})
+
+        write(
+            // feature data
+            [{ id: 0 }],
+            // geometry type
+            'POLYGON',
+            // geometries
+            [points],
+            function(err, files){
+                fs.writeFileSync('polygon.shp', toBuffer(files.shp.buffer));
+                fs.writeFileSync('polygon.shx', toBuffer(files.shx.buffer));
+                fs.writeFileSync('polygon.dbf', toBuffer(files.dbf.buffer));
+                var view = files.shp
+                describe('the file', function(){
+                    it('has the correct header', function(){
+                        expect(view.getInt32(0)).to.eql(9994); // FILE CODE
+
+
+                        expect(view.getInt32(28, true)).to.be(1000); // VERSION
+                        expect(view.getInt32(32, true)).to.be(types.geometries['POLYGON']) // SHAPE TYPE
+                    })
+
+                    it('has the correct bounds', function(){
+                        expect(view.getFloat64(36, true)).to.eql(xmin) // EXTENT
+                        expect(view.getFloat64(44, true)).to.eql(ymin)
+                        expect(view.getFloat64(52, true)).to.eql(xmax)
+                        expect(view.getFloat64(60, true)).to.eql(ymax)
+                    })
+
+                })
+                describe('the shape', function(){
+                    describe('the shape header', function(){
+                        it('should start record numbers at one', function(){
+                            expect(view.getInt32(100 + 0)).to.eql(1) // record number
+                        })
+
+                    })
+                    describe('the polygon', function(){
+                        describe('the header', function(){
+                            it('should be a polygon', function(){
+                                expect(view.getInt32(100 + 8 + 0, true)).to.eql(types.geometries['POLYGON']) // POLYLINE=3
+                            })
+                            it('should have the correct bounds', function(){
+                                expect(view.getFloat64(100 + 8 + 4, true)).to.eql(xmin) // EXTENT
+                                expect(view.getFloat64(100 + 8 + 12, true)).to.eql(ymin)
+                                expect(view.getFloat64(100 + 8 + 20, true)).to.eql(xmax)
+                                expect(view.getFloat64(100 + 8 + 28, true)).to.eql(ymax)
+                            })
+                        })
+                        describe('the counts', function(){
+                            it('should have one part', function(){
+                                expect(view.getInt32(100 + 8 + 36, true)).to.eql(1)
+                            })
+                            it('should have 4 points', function(){
+                                expect(view.getInt32(100 + 8 + 40, true)).to.eql(4)
+                            })
+                        })
+                        describe('the part ids', function(){
+                            it('should name the first part 0', function(){
+                                expect(view.getInt32(100 + 8 + 44, true)).to.eql(0)
+                            })
+                        })
+                        describe('the points', function(){
+                            it('should list the points correctly', function(){
+                                expect(view.getFloat64(100 + 8 + 44 + (4 * 1) + 0, true)).to.eql(149.2822265625)
+                                expect(view.getFloat64(100 + 8 + 44 + (4 * 1) + 8, true)).to.eql(-31.222197032103185)
+                                expect(view.getFloat64(100 + 8 + 44 + (4 * 1) + 16, true)).to.eql(150.00732421875)
+                                expect(view.getFloat64(100 + 8 + 44 + (4 * 1) + 24, true)).to.eql(-32.30570601389429)
+                                expect(view.getFloat64(100 + 8 + 44 + (4 * 1) + 32, true)).to.eql(147.89794921875)
+                                expect(view.getFloat64(100 + 8 + 44 + (4 * 1) + 40, true)).to.eql(-32.34284135639301)
+                                expect(view.getFloat64(100 + 8 + 44 + (4 * 1) + 48, true)).to.eql(149.2822265625)
+                                expect(view.getFloat64(100 + 8 + 44 + (4 * 1) + 56, true)).to.eql(-31.222197032103185)
+                            })  
+                        })
+                    })
+                    describe('the shape header', function(){
+                        var count
+                        it('should have the right shape content length', function(){
+                             count = 44 +/*to parts*/
+                                4/*one part*/ +
+                                (8 * 8)/*8 coords*/
+
+                            expect(view.getInt32(100 + 4)).to.eql(count / 2) //TODO
+                        })
+                        it('should have the right file length', function(){
+                            expect(view.getInt32(24)).to.be((
+                                100 +// file header
+                                8 + // shape header
+                                count // shape size
+                                ) / 2
+                            );
+                        })
+                    })
+                })
+            }
+        );
+    })
+
+    describe('multipolygon', function(){
+        var points =  [
+            [
                 [
-                    [
-                        149.2822265625,
-                        -31.222197032103185
-                    ],
-                    [
-                        150.00732421875,
-                        -32.30570601389429
-                    ],
-                    [
-                        147.89794921875,
-                        -32.34284135639301
-                    ],
-                    [
-                        149.2822265625,
-                        -31.222197032103185
-                    ]
+                    150.4248046875, -32.472695022061494
+                ],
+                [
+                    151.171875, -34.089061315849946
+                ],
+                [
+                    148.974609375, -34.107256396631186
+                ],
+                [
+                    150.4248046875, -32.472695022061494
                 ]
-            ];
+            ], [
+                [
+                    146.71142578125, -33.925129700071984
+                ],
+                [
+                    145.04150390625, -33.100745405144245
+                ],
+                [
+                    148.974609375, -32.39851580247402
+                ],
+                [
+                    146.71142578125, -33.925129700071984
+                ]
+            ]
+        ];
 
-            write(
-                // feature data
-                [{ id: 0 }],
-                // geometry type
-                'POLYGON',
-                // geometries
-                points,
-                function(err, files){
-                    var shpView = files.shp
-                    console.log("shpView", shpView)
-                    // expect(shpView.getInt32(1)).to.eql(1) // record number
-                    expect(shpView.getInt32(2)).to.eql(1) // record number
+        var xmin = Infinity
+        points.forEach(function(d){ d.forEach(function(d){ xmin = d[0] < xmin ? d[0] : xmin}) })
+        var ymin = Infinity
+        points.forEach(function(d){ d.forEach(function(d){ ymin = d[1] < ymin ? d[1] : ymin}) })
+        var xmax = -Infinity
+        points.forEach(function(d){ d.forEach(function(d){ xmax = d[0] > xmax ? d[0] : xmax}) })
+        var ymax = -Infinity
+        points.forEach(function(d){ d.forEach(function(d){ ymax = d[1] > ymax ? d[1] : ymax}) })
 
-                    // expect(shpView.getInt32(memoryInc, contentLength / 2)).to.eql(124142) // length // XXXX
-                    // expect(shpView.getInt32(memoryInc, TYPE, true)).to.eql(3) // POLYLINE=3
-                    // expect(shpView.getFloat64(memoryInc, xmin, true)).to.eql() // EXTENT
-                    // expect(shpView.getFloat64(memoryInc, ymin, true)).to.eql()
-                    // expect(shpView.getFloat64(memoryInc, xmax, true)).to.eql()
-                    // expect(shpView.getFloat64(memoryInc, ymax, true)).to.eql()
-                    // expect(shpView.getInt32(memoryInc, parts, true)).to.eql() // PARTS=t.geometry.coordinates.length
-                    // expect(shpView.getInt32(memoryInc, flattened.length, true)).to.eql() // POINTS
-                }
-            );
+        write(
+            // feature data
+            [{ id: 0 }],
+            // geometry type
+            'POLYGON',
+            // geometries
+            [points],
+            function(err, files){
+                fs.writeFileSync('multipolygon.shp', toBuffer(files.shp.buffer));
+                fs.writeFileSync('multipolygon.shx', toBuffer(files.shx.buffer));
+                fs.writeFileSync('multipolygon.dbf', toBuffer(files.dbf.buffer));
+                var view = files.shp
 
-        });
-    });
+                describe('the file', function(){
+                    it('has the correct header', function(){
+                        expect(view.getInt32(0)).to.eql(9994); // FILE CODE
+
+                        expect(view.getInt32(28, true)).to.be(1000); // VERSION
+                        expect(view.getInt32(32, true)).to.be(types.geometries['POLYGON']) // SHAPE TYPE
+                    })
+
+                    it('has the correct bounds', function(){
+                        expect(view.getFloat64(36, true)).to.eql(xmin) // EXTENT
+                        expect(view.getFloat64(44, true)).to.eql(ymin)
+                        expect(view.getFloat64(52, true)).to.eql(xmax)
+                        expect(view.getFloat64(60, true)).to.eql(ymax)
+                    })
+
+                })
+                describe('the shape', function(){
+                    describe('the shape header', function(){
+                        it('should start record numbers at one', function(){
+                            expect(view.getInt32(100 + 0)).to.eql(1) // record number
+                        })
+
+                    })
+                    describe('the polygon', function(){
+                        describe('the header', function(){
+                            it('should be a polygon', function(){
+                                expect(view.getInt32(100 + 8 + 0, true)).to.eql(types.geometries['POLYGON']) // POLYLINE=3
+                            })
+                            it('should have the correct bounds', function(){
+                                expect(view.getFloat64(100 + 8 + 4, true)).to.eql(xmin) // EXTENT
+                                expect(view.getFloat64(100 + 8 + 12, true)).to.eql(ymin)
+                                expect(view.getFloat64(100 + 8 + 20, true)).to.eql(xmax)
+                                expect(view.getFloat64(100 + 8 + 28, true)).to.eql(ymax)
+                            })
+                        })
+                        describe('the counts', function(){
+                            it('should have 2 parts', function(){
+                                expect(view.getInt32(100 + 8 + 36, true)).to.eql(2)
+                            })
+                            it('should have 8 points', function(){
+                                expect(view.getInt32(100 + 8 + 40, true)).to.eql(8)
+                            })
+                        })
+                        describe('the part indexes', function(){
+                            it('the first part starts at 0', function(){
+                                expect(view.getInt32(100 + 8 + 44, true)).to.eql(0)
+                            })
+                            it('the second part starts at 4', function(){
+                                expect(view.getInt32(100 + 8 + 48, true)).to.eql(4)
+                            })
+                        })
+                        describe('the points', function(){
+                            it('should list the points correctly', function(){
+                                expect(view.getFloat64(100 + 8 + 44 + (4 * 2) + (8 * 0), true)).to.eql(150.4248046875)
+                                expect(view.getFloat64(100 + 8 + 44 + (4 * 2) + (8 * 1), true)).to.eql(-32.472695022061494)
+                                expect(view.getFloat64(100 + 8 + 44 + (4 * 2) + (8 * 2), true)).to.eql(151.171875)
+                                expect(view.getFloat64(100 + 8 + 44 + (4 * 2) + (8 * 3), true)).to.eql(-34.089061315849946)
+                                expect(view.getFloat64(100 + 8 + 44 + (4 * 2) + (8 * 4), true)).to.eql(148.974609375)
+                                expect(view.getFloat64(100 + 8 + 44 + (4 * 2) + (8 * 5), true)).to.eql(-34.107256396631186)
+                                expect(view.getFloat64(100 + 8 + 44 + (4 * 2) + (8 * 6), true)).to.eql(150.4248046875)
+                                expect(view.getFloat64(100 + 8 + 44 + (4 * 2) + (8 * 7), true)).to.eql(-32.472695022061494)
+                                expect(view.getFloat64(100 + 8 + 44 + (4 * 2) + (8 * 8), true)).to.eql(146.71142578125)
+                                expect(view.getFloat64(100 + 8 + 44 + (4 * 2) + (8 * 9), true)).to.eql(-33.925129700071984)
+                                expect(view.getFloat64(100 + 8 + 44 + (4 * 2) + (8 * 10), true)).to.eql(145.04150390625)
+                                expect(view.getFloat64(100 + 8 + 44 + (4 * 2) + (8 * 11), true)).to.eql(-33.100745405144245)
+                                expect(view.getFloat64(100 + 8 + 44 + (4 * 2) + (8 * 12), true)).to.eql(148.974609375)
+                                expect(view.getFloat64(100 + 8 + 44 + (4 * 2) + (8 * 13), true)).to.eql(-32.39851580247402)
+                                expect(view.getFloat64(100 + 8 + 44 + (4 * 2) + (8 * 14), true)).to.eql(146.71142578125)
+                                expect(view.getFloat64(100 + 8 + 44 + (4 * 2) + (8 * 15), true)).to.eql(-33.925129700071984)
+                            })  
+                        })
+                    })
+                    describe('the shape header', function(){
+                        var count
+                        it('should have the right shape content length', function(){
+                             count = 44 +/*poly header up till parts*/
+                                (4 * 2)/*two part*/ +
+                                (8 * 16)/*16 coords*/
+
+                            expect(view.getInt32(100 + 4)).to.eql(count / 2) //TODO
+                        })
+                        it('should have the right file length', function(){
+                            expect(view.getInt32(24)).to.be((
+                                100 +// file header
+                                8 + // shape header
+                                count // shape size
+                                ) / 2
+                            );
+                        })
+                    })
+                })
+            }
+        );
+    })
 });
 
 
@@ -122,3 +346,11 @@ describe('extent', function() {
 
 
 */
+
+
+function toBuffer(ab) {
+    var buffer = new Buffer(ab.byteLength),
+        view = new Uint8Array(ab);
+    for (var i = 0; i < buffer.length; ++i) { buffer[i] = view[i]; }
+    return buffer;
+}
